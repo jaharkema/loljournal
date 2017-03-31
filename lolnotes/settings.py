725500aeada9321b140da36ee14a80bd5b782f6e
Jan Harkema <jharkema@ilionx.com>
2017-03-31 16:40:18 +0200

    Setting DEBUG=True

--- lolnotes/settings.py
+++ lolnotes/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 MEDIA_ROOT = PROJECT_DIR('media')
 MEDIA_URL = '/media/'
